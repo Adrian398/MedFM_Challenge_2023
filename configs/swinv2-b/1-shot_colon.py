f0a495d3f885dfffc69de42e2f7aa6171f39f107
Marcel Roth <MarcelRoth100@aol.com>
2023-08-20 21:46:28 +0200

    update swinv2-b 1-shot colon config

--- configs/swinv2-b/1-shot_colon.py
+++ configs/swinv2-b/1-shot_colon.py
@@ -38,11 +38,13 @@ train_pipeline = [
 
 train_dataloader = dict(
     batch_size=train_bs,
+    num_workers=1,
     dataset=dict(ann_file=f'data_anns/MedFMC/{dataset}/{dataset}_{nshot}-shot_train_exp{exp_num}.txt'),
 )
 
 val_dataloader = dict(
     batch_size=128,
+    num_workers=1,
     dataset=dict(ann_file=f'data_anns/MedFMC/{dataset}/{dataset}_{nshot}-shot_val_exp{exp_num}.txt'),
 )
 
