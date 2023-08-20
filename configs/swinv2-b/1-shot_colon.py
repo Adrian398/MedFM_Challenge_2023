6bf1e7544ef173e335cee77235ac16cd8fdaa234
Marcel Roth <MarcelRoth100@aol.com>
2023-08-20 21:39:32 +0200

    add swinv2-b 1-shot colon config

---
+++ configs/swinv2-b/1-shot_colon.py
@@ -0,0 +1,65 @@
+_base_ = [
+    'mmpretrain::_base_/models/swin_transformer_v2/base_384.py',
+    '../datasets/colon.py',
+    'mmpretrain::_base_/schedules/imagenet_bs1024_adamw_swin.py',
+    'mmpretrain::_base_/default_runtime.py',
+    '../custom_imports.py'
+]
+
+lr = 5e-2
+train_bs = 8
+dataset = 'colon'
+model_name = 'swinv2'
+exp_num = 1
+nshot = 1
+#
+run_name = f'{model_name}_bs{train_bs}_lr{lr}_exp{exp_num}_'
+work_dir = f'work_dirs/colon/{nshot}-shot/{run_name}'
+
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(
+        img_size=384,
+        window_size=[24, 24, 24, 12],
+        drop_path_rate=0.2,
+        pretrained_window_sizes=[12, 12, 12, 6]))
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='RandomResizedCrop',
+        scale=384,
+        backend='pillow',
+        interpolation='bicubic'),
+    dict(type='RandomFlip', prob=0.5, direction='horizontal'),
+    dict(type='RandomFlip', prob=0.5, direction='vertical'),
+    dict(type='PackInputs'),
+]
+
+train_dataloader = dict(
+    batch_size=train_bs,
+    dataset=dict(ann_file=f'data_anns/MedFMC/{dataset}/{dataset}_{nshot}-shot_train_exp{exp_num}.txt'),
+)
+
+val_dataloader = dict(
+    batch_size=128,
+    dataset=dict(ann_file=f'data_anns/MedFMC/{dataset}/{dataset}_{nshot}-shot_val_exp{exp_num}.txt'),
+)
+
+val_evaluator = [
+    dict(type='AveragePrecision'),
+    dict(type='MultiLabelMetric', average='macro'),  # class-wise mean
+    dict(type='MultiLabelMetric', average='micro'),  # overall mean
+    dict(type='Accuracy', topk=(1,)),
+    dict(type='AUC')
+]
+test_evaluator = val_evaluator
+
+default_hooks = dict(
+    checkpoint=dict(type='CheckpointHook', interval=250, max_keep_ckpts=1, save_best="auto"),
+    logger=dict(interval=10),
+)
+
+visualizer = dict(type='Visualizer', vis_backends=[dict(type='TensorboardVisBackend')])
+
+train_cfg = dict(by_epoch=True, val_interval=125, max_epochs=1500)
