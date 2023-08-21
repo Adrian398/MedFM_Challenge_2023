d40a054c15006148e9a1c46ee28691333cd93662
Marcel Roth <MarcelRoth100@aol.com>
2023-08-21 21:49:14 +0200

    update swinv2 model

--- configs/swinv2-b/1-shot_colon.py
+++ configs/swinv2-b/1-shot_colon.py
@@ -1,7 +1,7 @@
 _base_ = [
     '../datasets/colon.py',
     '../swin_schedule.py',
-    '../_base_/models/swin_transformer_v2/base_384.py',
+    'mmpretrain::_base_/models/swin_transformer_v2/base_384.py',
     #'mmpretrain::_base_/schedules/imagenet_bs1024_adamw_swin.py',
     'mmpretrain::_base_/default_runtime.py',
     '../custom_imports.py'
