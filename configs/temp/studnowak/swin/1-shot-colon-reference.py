69ec87a417a1f91bf727ab4b6561101fdb04e812
Micha Nowak <gonderjake@gmail.com>
2023-08-21 23:01:14 +0200

    update 1-shot colon reference

--- configs/temp/studnowak/swin/1-shot-colon-reference.py
+++ configs/temp/studnowak/swin/1-shot-colon-reference.py
@@ -1,7 +1,14 @@
 _base_ = [
     'mmpretrain::_base_/default_runtime.py',
-    '../custom_imports.py',
 ]
+
+custom_imports = dict(
+    imports=[
+        'medfmc.datasets.medical_datasets',
+        'medfmc.evaluation.metrics.auc',
+        'medfmc.models'
+    ],
+    allow_failed_imports=False)
 
 # general
 task = 'colon'
