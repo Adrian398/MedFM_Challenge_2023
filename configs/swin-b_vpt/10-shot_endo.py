6e31ae4f6de40b77577cacc21b00b062214eedaf
Adrian39 <adrian@stellaris.de>
2023-08-21 20:11:47 +0200

    change bug endo 10 shot

--- configs/swin-b_vpt/10-shot_endo.py
+++ configs/swin-b_vpt/10-shot_endo.py
@@ -30,7 +30,7 @@ model = dict(
         init_cfg=dict(
             type='Pretrained',
             checkpoint=
-            'https://download.openmmlab.com/mmclassification/v0/swin-transformer/convert/swin_base_patch4_window12_384_22kto1k>
+            'https://download.openmmlab.com/mmclassification/v0/swin-transformer/convert/swin_base_patch4_window12_384_22kto1k-d59b0d1d.pth',
             prefix='backbone',
         ),
         stage_cfgs=dict(block_cfgs=dict(window_size=12))),
