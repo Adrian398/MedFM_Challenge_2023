ebba94fed4ac2080cc7592a41aabf4f15b08860e
AmarHek <amar.hekalo@uni-wuerzburg.de>
2023-08-21 13:58:57 +0200

    added data augmentations to dataset/chest.py and removed from config

--- configs/datasets/chest.py
+++ configs/datasets/chest.py
@@ -12,9 +12,13 @@ data_preprocessor = dict(
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
+    dict(type='NumpyToPIL', to_rgb=True),
+    dict(type='torchvision/RandomAffine', degrees=(-15, 15), translate=(0.05, 0.05), fill=128),
+    dict(type='PILToNumpy', to_bgr=True),
     dict(
         type='RandomResizedCrop',
         scale=384,
+        crop_ratio_range=(0.9, 1.0),
         backend='pillow',
         interpolation='bicubic'),
     dict(type='RandomFlip', prob=0.5, direction='horizontal'),
