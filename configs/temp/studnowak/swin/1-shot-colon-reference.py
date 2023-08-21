0a25c5dde9a72e9fcb9793bca4bd0e2fd6af5dce
Micha Nowak <gonderjake@gmail.com>
2023-08-21 23:59:45 +0200

    update 1-shot colon reference, fix data_prefix

--- configs/temp/studnowak/swin/1-shot-colon-reference.py
+++ configs/temp/studnowak/swin/1-shot-colon-reference.py
@@ -12,6 +12,7 @@ custom_imports = dict(
 
 # general
 task = 'colon'
+dataset_type = 'Colon'
 model_name = 'swin'
 exp_num = 1
 n_shot = 1
@@ -41,6 +42,7 @@ train_dataloader = dict(
     batch_size=train_bs,
     dataset=dict(ann_file=f'data_anns/MedFMC/{task}/{task}_{n_shot}-shot_train_exp{exp_num}.txt',
                  data_prefix=data_prefix,
+                 dataset_type=dataset_type,
                  pipeline=train_pipeline),
 )
 
@@ -54,6 +56,7 @@ val_dataloader = dict(
     batch_size=64,
     dataset=dict(pipeline=test_pipeline,
                  data_prefix=data_prefix,
+                 dataset_type=dataset_type,
                  ann_file=f'data_anns/MedFMC/{task}/{task}_{n_shot}-shot_val_exp{exp_num}.txt'),
 )
 
@@ -61,6 +64,7 @@ test_dataloader = dict(
     batch_size=4,
     dataset=dict(
         data_prefix=data_prefix,
+        dataset_type=dataset_type,
         ann_file=f'data_anns/MedFMC/{task}/test_WithLabel.txt'),
 )
 
