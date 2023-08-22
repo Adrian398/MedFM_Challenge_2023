6af6f49aba424c6b733200d3b1c533ad8ad2efcd
Amar Hekalo <amar.hekalo@uni-wuerzburg.de>
2023-08-22 13:40:52 +0200

    max default epochs 500 -> 1000

--- configs/schedules/chest.py
+++ configs/schedules/chest.py
@@ -32,7 +32,7 @@ param_scheduler = [
 ]
 
 # train, val, test setting
-train_cfg = dict(by_epoch=True, val_interval=1, max_epochs=500)
+train_cfg = dict(by_epoch=True, val_interval=1, max_epochs=1000)
 val_cfg = dict()
 test_cfg = dict()
 
