d4bfe5bb64e30ff56c394ad16633e4553b86a37c
Micha Nowak <gonderjake@gmail.com>
2023-08-22 03:19:23 +0200

    update autoinfer

--- auto_infer.py
+++ auto_infer.py
@@ -4,6 +4,19 @@ import shutil
 import subprocess
 import sys
 
+
+def find_repo(root_dir, target):
+    for root, dirs, _ in os.walk(root_dir):
+        if target in dirs:
+            return os.path.join(root, target)
+    return None
+
+
+home_directory = os.path.expanduser("~")
+target_directory = "medfm-challenge"
+
+home_repo_path = find_repo(home_directory, target_directory)
+
 path = sys.argv[1]
 delimiter = "/"
 path = os.path.join("work_dirs", path)
@@ -18,21 +31,23 @@ checkpoint_filename = [file for file in os.listdir(run_dir) if file.endswith(".p
 config_path = os.path.join(run_dir, config_filename)
 checkpoint_path = os.path.join(run_dir, checkpoint_filename)
 images_path = f"data/MedFMC_val/{task}/images"
-out_path = f"results/{task}_{shot}_submission.csv"
+csv_name = f"{task}_{shot}_submission.csv"
+out_path = os.path.join(home_repo_path, "results", csv_name)
 
 # create dir for submission and config
-submission_path = "submissions"
+submission_path = os.path.join(home_repo_path, "submissions")
 date_pattern = datetime.now().strftime("%d-%m")
 submission_dir = os.path.join(submission_path, date_pattern)
 configs_dir = os.path.join(submission_dir, "configs")
 if not any(s.startswith(date_pattern) for s in os.listdir(submission_path)):
     os.makedirs(submission_dir)
     print(f"Directory {submission_dir} created.")
-    os.makedirs(configs_dir)
+    # os.makedirs(configs_dir)
 else:
     print(f"Directory with pattern {date_pattern} already exists.")
 
+print(f"Copying config from {config_path} to {configs_dir}")
+print(f"Starting infer with {config_path} {checkpoint_path} {images_path} {out_path}")
 # copy config into directory
-shutil.copy(config_path, configs_dir)
-subprocess.run(["python", "tools/infer.py", config_path, checkpoint_path, images_path, out_path])
-
+# shutil.copy(config_path, configs_dir)
+# subprocess.run(["python", "tools/infer.py", config_path, checkpoint_path, images_path, out_path])
