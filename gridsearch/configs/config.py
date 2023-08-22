c9e381784ea72076bc6e23ce47a8966e54bbf2fd
Marcel Roth <MarcelRoth100@aol.com>
2023-08-22 22:45:45 +0200

    update grid search approach

--- gridsearch/configs/config.py
+++ gridsearch/configs/config.py
@@ -1,13 +1,13 @@
 OVERRIDE = {
-    'model': ["swinv2-b"],
+    'model': ["swinv2-b", "clip-b_vpt"],
     'dataset': "colon",
     'shot': 1,
     'exp_num': [1,2,3,4,5],
-    'lr': 1e-6
+    'lr': 1e-4
 }
 
 SETTINGS = {
-    'exp_suffix': "swinv2-exp-test_",
+    'exp_suffix': "exp-test_",
     # Use dry run to only generate the python commands but not execute them
     'dry_run': False,
     'log_level': "INFO"
