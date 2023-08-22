a79b53f8adf5bf0e8f469afd994112517ba500e3
Micha Nowak <gonderjake@gmail.com>
2023-08-22 03:46:30 +0200

    update auto infer

--- auto_infer.py
+++ auto_infer.py
@@ -23,6 +23,10 @@ checkpoint_path = os.path.join(run_dir, checkpoint_filename)
 images_path = f"data/MedFMC_val/{task}/images"
 csv_name = f"{task}_{shot}_submission.csv"
 out_path = os.path.join("results", csv_name)
+print(task)
+print(shot)
+print(csv_name)
+print(out_path)
 
 # create dir for submission and config
 submission_path = os.path.join("submissions")
@@ -30,14 +34,14 @@ date_pattern = datetime.now().strftime("%d-%m")
 submission_dir = os.path.join(submission_path, date_pattern)
 configs_dir = os.path.join(submission_dir, "configs")
 if not any(s.startswith(date_pattern) for s in os.listdir(submission_path)):
-    os.makedirs(submission_dir)
+    # os.makedirs(submission_dir)
     print(f"Directory {submission_dir} created.")
-    os.makedirs(configs_dir)
+    # os.makedirs(configs_dir)
 else:
     print(f"Directory with pattern {date_pattern} already exists.")
 
 print(f"Copying config from {config_path} to {configs_dir}")
 print(f"Starting infer with {config_path} {checkpoint_path} {images_path} {out_path}")
 # copy config into directory
-shutil.copy(config_path, configs_dir)
-subprocess.run(["python", "tools/infer.py", config_path, checkpoint_path, images_path, "--out", out_path])
+# shutil.copy(config_path, configs_dir)
+# subprocess.run(["python", "tools/infer.py", config_path, checkpoint_path, images_path, "--out", out_path])
