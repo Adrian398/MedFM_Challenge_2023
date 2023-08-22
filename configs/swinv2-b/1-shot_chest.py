a229f38e5de29fa20693f9761b8a6ae4f82b5231
Amar Hekalo <amar.hekalo@uni-wuerzburg.de>
2023-08-22 15:22:53 +0200

    added chest configs for swinv2

---
+++ configs/swinv2-b/1-shot_chest.py
@@ -0,0 +1,71 @@
+_base_ = [
+    '../datasets/chest.py',
+    '../schedules/chest.py',
+    'mmpretrain::_base_/default_runtime.py',
+    'mmpretrain::_base_/models/swin_transformer_v2/base_384.py',
+    '../custom_imports.py',
+]
+
+lr = 5e-2
+train_bs = 8
+dataset = 'chest'
+model_name = 'swinv2'
+exp_num = 2
+nshot = 1
+
+run_name = f'{model_name}_bs{train_bs}_lr{lr}_exp{exp_num}_'
+work_dir = f'work_dirs/chest/{nshot}-shot/{run_name}'
+
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(
+        img_size=384,
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint=
+            'https://download.openmmlab.com/mmclassification/v0/swin-v2/swinv2-base-w24_in21k-pre_3rdparty_in1k-384px_20220803-44eb70f8.pth',
+            prefix='backbone',
+        ),
+        window_size=[24, 24, 24, 12],
+        drop_path_rate=0.2,
+        pretrained_window_sizes=[12, 12, 12, 6]),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='LinearClsHead',
+        num_classes=19,
+        in_channels=1024,
+        loss=dict(type='LabelSmoothLoss', loss_weight=1.0),
+    )
+)
+
+train_pipeline = [
+    dict(type='RandomResizedCrop',
+         scale=1024,
+         crop_ratio_range=(0.9, 1.0),
+         backend='pillow',
+         interpolation='bicubic'),
+    ]
+
+train_dataloader = dict(
+    batch_size=4, 
+    dataset=dict(ann_file=f'data_anns/MedFMC/{dataset}/{dataset}_{nshot}-shot_train_exp{exp_num}.txt'),
+)
+
+val_dataloader = dict(
+    batch_size=8,  
+    dataset=dict(ann_file=f'data_anns/MedFMC/{dataset}/{dataset}_{nshot}-shot_val_exp{exp_num}.txt'),
+)
+
+test_dataloader = dict(
+    batch_size=4,  
+    dataset=dict(ann_file=f'data_anns/MedFMC/{dataset}/test_WithLabel.txt'),
+)
+
+optim_wrapper = dict(optimizer=dict(lr=lr))
+
+default_hooks = dict(
+    checkpoint=dict(type='CheckpointHook', interval=1, max_keep_ckpts=1, save_best="auto"),
+    logger=dict(interval=50),
+)
+
+visualizer = dict(type='Visualizer', vis_backends=[dict(type='TensorboardVisBackend')])
