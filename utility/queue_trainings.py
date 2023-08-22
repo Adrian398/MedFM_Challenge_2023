6b12a07cad4e543c7dfd3093a28cfba46d9a2951
Marcel Roth <MarcelRoth100@aol.com>
2023-08-22 19:40:50 +0200

    update grid search approach

--- utility/queue_trainings.py
+++ utility/queue_trainings.py
@@ -1,4 +1,7 @@
+import logging
 import subprocess
+
+logging.basicConfig(level=logging.INFO)
 
 BASE_PARAMS_CONFIG = {
     'model': ["clip-b_vpt", "dinov2-b_vpt", "eva-b_vpt", "swin-b_vpt", "swinv2-b", "vit-b_vpt"],
@@ -11,14 +14,16 @@ BASE_PARAMS_CONFIG = {
 
 def run_training(params):
     cfg_path = generate_config_path(params['model'], params['shot'], params['dataset'])
-    command = ["python", "tools/train.py", ]
+    command = ["python", "tools/train.py", cfg_path]
 
     for key, value in params.items():
         # only add params as command arg if required
         if key not in ['model', 'shot', 'dataset']:
             command.extend([f"--{key}", str(value)])
-    subprocess.run(command)
 
+    logging.info(f"Starting training with config: {params}")
+    subprocess.run(command)
+    logging.info(f"Training for config {params} completed")
 
 def generate_config_path(model, shot, dataset):
     return f"configs/{model}/{shot}-shot_{dataset}.py"
@@ -47,6 +52,7 @@ if __name__ == "__main__":
     USER_OVERRIDE = {
         'model': ['swinv2-b'],
         'dataset': ['colon'],
+        'exp_suffix': [1],
         'shot': [1],
         'lr': [1e-6]
     }
