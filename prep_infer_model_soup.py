4d922bc07ab39b767b5b93d55449493313b98c61
Adrian39 <adrian@stellaris.de>
2023-08-24 21:29:35 +0200

    add

--- prep_infer_model_soup.py
+++ prep_infer_model_soup.py
@@ -2,7 +2,10 @@ import torch
 import os
 
 def get_sd(state_dicts, alphal):
-  sd = {k : state_dicts[0][k].clone() * alphal[0] for k in state_dicts[0].keys()}
+  sd = {}  # Initialize an empty dictionary
+  for k in state_dicts[0].keys():
+      print(state_dicts[0][k])
+      sd[k] = state_dicts[0][k].clone() * alphal[0]
   for i in range(1, len(state_dicts)):
       for k in state_dicts[i].keys():
           sd[k] = sd[k] + state_dicts[i][k].clone() * alphal[i]
@@ -21,7 +24,6 @@ print(type(state_dicts[0]))
 
 
 alphal = [1 / len(state_dicts) for i in range(len(state_dicts))]
-print(alphal)
 sd = get_sd(state_dicts, alphal)
 
 folder_path =  checkpoint_filenames[0].split("-shot")[0] + "-shot/modelsoup"
