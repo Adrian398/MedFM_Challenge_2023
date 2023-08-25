6d91e31ef9927959c94c56c0b8a042605987e0e2
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2023-08-25 16:56:48 +0200

    update

--- configs/swinv2-b/1-shot_colon.py
+++ configs/swinv2-b/1-shot_colon.py
@@ -14,7 +14,7 @@ exp_num = 1
 nshot = 1
 
 run_name = f'{model_name}_bs{train_bs}_lr{lr}_exp{exp_num}_'
-work_dir = f'work_dirs/colon/{nshot}-shot/{run_name}'
+work_dir = f'work_dirs/{dataset}/{nshot}-shot/{run_name}'
 
 model = dict(
     _scope_='mmpretrain',
