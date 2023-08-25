da20167d149eadbbb491b6783717acfe542d1d15
Marcel Roth <MarcelRoth100@aol.com>
2023-08-25 14:35:55 +0200

    update gridsearch cfg

--- configs/swinv2-b/10-shot_endo.py
+++ configs/swinv2-b/10-shot_endo.py
@@ -123,46 +123,57 @@ test_pipeline = [
 ]
 train_bs = 8
 train_cfg = dict(by_epoch=True, max_epochs=200, val_interval=25)
+# train_pipeline = [
+#             dict(type='LoadImageFromFile'),
+#             dict(
+#                 type='Normalize',
+#                 mean=mean,
+#                 std=std,
+#                 to_rgb=False
+#             ),
+#             dict(
+#                 backend='pillow',
+#                 interpolation='bicubic',
+#                 scale=384,
+#                 type='Resize'),
+#             dict(
+#                 type='RandomResizedCrop',
+#                 scale=384,
+#                 backend='pillow',
+#                 interpolation='bicubic'),
+#             dict(type='RandomFlip', prob=0.5, direction='horizontal'),
+#             dict(type='RandomFlip', prob=0.5, direction='vertical'),
+#             dict(
+#                 type='ColorJitter',
+#                 brightness=0.2,
+#                 contrast=(0.8, 1.2),
+#                 saturation=0.3,
+#                 hue=(-0.1, 0.1),
+#                 backend='pillow'
+#             ),
+#             dict(
+#                 type='Cutout',
+#                 shape=(64, 64),
+#                 pad_val=128,
+#                 prob=0.5
+#             ),
+#             dict(
+#                 type='Rotate',
+#                 angle=10
+#             ),
+#             dict(type='PackInputs'),
+#         ]
 train_pipeline = [
-            dict(type='LoadImageFromFile'),
-            dict(
-                type='Normalize',
-                mean=mean,
-                std=std,
-                to_rgb=False
-            ),
-            dict(
-                backend='pillow',
-                interpolation='bicubic',
-                scale=384,
-                type='Resize'),
-            dict(
-                type='RandomResizedCrop',
-                scale=384,
-                backend='pillow',
-                interpolation='bicubic'),
-            dict(type='RandomFlip', prob=0.5, direction='horizontal'),
-            dict(type='RandomFlip', prob=0.5, direction='vertical'),
-            dict(
-                type='ColorJitter',
-                brightness=0.2,
-                contrast=(0.8, 1.2),
-                saturation=0.3,
-                hue=(-0.1, 0.1),
-                backend='pillow'
-            ),
-            dict(
-                type='Cutout',
-                shape=(64, 64),
-                pad_val=128,
-                prob=0.5
-            ),
-            dict(
-                type='Rotate',
-                angle=10
-            ),
-            dict(type='PackInputs'),
-        ]
+    dict(type='LoadImageFromFile'),
+    dict(backend='pillow', interpolation='bicubic', scale=384, type='Resize'),
+    dict(
+        type='Normalize',
+        mean=mean,
+        std=std,
+        to_rgb=False
+    ),
+    dict(type='PackInputs'),
+]
 train_dataloader = dict(
     batch_size=train_bs,
     collate_fn=dict(type='default_collate'),
