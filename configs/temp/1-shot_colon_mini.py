431f55d7d9d5f4666010d44cbdc138968d6a435c
Micha Nowak <gonderjake@gmail.com>
2023-08-27 05:05:37 +0200

    update

--- configs/temp/1-shot_colon_mini.py
+++ configs/temp/1-shot_colon_mini.py
@@ -101,7 +101,7 @@ val_evaluator = [
 test_evaluator = val_evaluator
 
 default_hooks = dict(
-    checkpoint=dict(type='CheckpointHook', interval=25000, max_keep_ckpts=0),
+    checkpoint=dict(type='CheckpointHook', interval=25000, max_keep_ckpts=-1),
     logger=dict(interval=10),
 )
 
@@ -130,5 +130,5 @@ param_scheduler = [
 ]
 
 randomness = dict(seed=0)
-train_cfg = dict(by_epoch=True, val_interval=5, max_epochs=50)
+train_cfg = dict(by_epoch=True, val_interval=10, max_epochs=100)
 auto_scale_lr = dict(base_batch_size=1024)
