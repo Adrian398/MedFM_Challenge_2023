0dfecdb7afbe7ac1f2756ab48f756fde7ea25153
Adrian39 <adrian@stellaris.de>
2023-08-28 15:30:05 +0200

    added models soup fix

--- prep_infer_model_soup.py
+++ prep_infer_model_soup.py
@@ -38,7 +38,7 @@ for dirpath, dirnames, filenames in os.walk(start_dir):
 
 print(checkpoint_filenames)
 
-checkpoint_filenames = checkpoint_filenames[:2]
+checkpoint_filenames = checkpoint_filenames[:3]
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 state_dicts = []
