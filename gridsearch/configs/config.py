41d05c360da22e69b5293bfa99602aa61a5064d1
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2023-08-28 18:26:39 +0200

    add seed to train.py and update configs

--- gridsearch/configs/config.py
+++ gridsearch/configs/config.py
@@ -5,7 +5,7 @@ OVERRIDE = {
     #'exp_num': [1],
     'lr': [1e-6],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
     'train_bs': [8],
-    'seed': [0]
+    'seed': [0, 42]
 }
 
 SETTINGS = {
