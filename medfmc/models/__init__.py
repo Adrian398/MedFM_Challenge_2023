c12a9f83103ef66fe979185c1731e901afa0f98a
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2023-08-30 18:03:40 +0200

    add prompted swin transformer v2 to init

--- medfmc/models/__init__.py
+++ medfmc/models/__init__.py
@@ -2,10 +2,12 @@ from .prompt_swin import PromptedSwinTransformer
 from .prompt_swin_custom import CustomPromptedSwinTransformer
 from .prompt_vit import PromptedViT
 from .prompt_eva import PromptedViTEVA02
+from .prompt_swinv2 import PromptedSwinTransformerV2
 
 __all__ = [
     'PromptedViT', 
     'PromptedSwinTransformer',
     'CustomPromptedSwinTransformer',
-    'PromptedViTEVA02'
+    'PromptedViTEVA02',
+    'PromptedSwinTransformerV2'
 ]
