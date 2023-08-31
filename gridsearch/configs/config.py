26616c01de1e1ce643a6206cd80206a95c80c68e
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2023-08-31 14:16:02 +0200

    update endo 1-shot cfg resnet

--- gridsearch/configs/config.py
+++ gridsearch/configs/config.py
@@ -2,13 +2,13 @@ OVERRIDE = {
     'model': ["resnet101"],
     'dataset': ["endo"],
     'shot': [1],
-    'exp_num': [3],
-    'lr': [1e-6, 5e-6, 5e-5],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
+    #'exp_num': [3],
+    'lr': [5e-6],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
     #'train_bs': [32],
-    'seed': [0]
+    'seed': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 }
 
 SETTINGS = {
-    'exp_suffix': "resnet_sanity_test_",
+    'exp_suffix': "seed_test_",
     'log_level': "INFO"
 }
