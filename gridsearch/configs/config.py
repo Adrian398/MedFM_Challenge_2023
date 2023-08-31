94e59a638e1235e139770dfd8823025b71211a06
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2023-08-31 17:11:25 +0200

    update prep_infer

--- gridsearch/configs/config.py
+++ gridsearch/configs/config.py
@@ -1,11 +1,11 @@
 OVERRIDE = {
     'model': ["resnet101"],
-    'dataset': ["endo"],
+    'dataset': ["colon"],
     'shot': [1],
-    #'exp_num': [3],
-    'lr': [5e-6],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
+    'exp_num': [2],
+    'lr': [1e-6],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
     #'train_bs': [32],
-    'seed': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    'seed': [0, 1, 2, 3, 4]
 }
 
 SETTINGS = {
