1b2b16066c85bc3598b9d3ff8e64690f3ca00e5c
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2023-08-31 23:03:37 +0200

    update gridsearch config

--- gridsearch/configs/config.py
+++ gridsearch/configs/config.py
@@ -1,14 +1,14 @@
 OVERRIDE = {
-    'model': ["swinv2"],
-    'dataset': ["colon"],
-    'shot': [1],
-    'exp_num': [2],
-    'lr': [1e-6],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
+    'model': ["resnet101"],
+    'dataset': ["endo"],
+    'shot': [5, 10],
+    #'exp_num': [2],
+    'lr': [5e-6],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
     #'train_bs': [32],
     'seed': [0, 1, 2, 3, 4]
 }
 
 SETTINGS = {
-    'exp_suffix': "seed_test_",
+    'exp_suffix': "exp+seed_test_",
     'log_level': "INFO"
 }
