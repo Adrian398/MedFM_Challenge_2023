c3de3a77737da289a0ea3df7327b9766f912a2de
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2023-08-31 13:12:26 +0200

    update endo 1-shot cfg resnet

--- configs/resnet101/1-shot_endo.py
+++ configs/resnet101/1-shot_endo.py
@@ -9,8 +9,8 @@ _base_ = [
 checkpoint = 'https://download.openmmlab.com/mmclassification/v0/resnet/resnet101_8xb32_in1k_20210831-539c63f8.pth'  # noqa
 
 lr = 1e-6
-train_bs = 32
-val_bs = 128
+train_bs = 16
+val_bs = 64
 dataset = 'endo'
 model_name = 'resnet101'
 exp_num = 1
