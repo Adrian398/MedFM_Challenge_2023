de76b81ecae5e65c6458889366c5edb3c2cb2878
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2023-08-31 16:35:14 +0200

    update prep_infer

--- configs/resnet101/1-shot_colon.py
+++ configs/resnet101/1-shot_colon.py
@@ -9,8 +9,8 @@ _base_ = [
 checkpoint = 'https://download.openmmlab.com/mmclassification/v0/resnet/resnet101_8xb32_in1k_20210831-539c63f8.pth'  # noqa
 
 lr = 1e-6
-train_bs = 2
-val_bs = 4
+train_bs = 32
+val_bs = 256
 dataset = 'colon'
 model_name = 'resnet101'
 exp_num = 4
