ba907dc967f5a6803309f43e3eda9d6b31fb3b06
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2023-08-31 15:12:58 +0200

    update prep_infer

--- prep_infer.py
+++ prep_infer.py
@@ -30,6 +30,7 @@ metric = metric_tags[metric]
 
 tasks = ["colon", "endo", "chest"]
 shots = ["1", "5", "10"]
+exps = [1, 2, 3, 4, 5]
 
 
 # DEBUG
@@ -92,30 +93,28 @@ def exclude_model(name):
     return False
 
 
-def get_best_run_dir(task, shot, metric):
+def filter_directories_by_exp(directories, exp_number):
+    exp_str = f"exp{exp_number}"
+    filtered_dirs = [dir for dir in directories if exp_str in dir]
+    return filtered_dirs
+
+
+def get_5_best_exp_run_dirs(task, shot, exp, metric):
     setting_directory = os.path.join(work_dir_path, task, f"{shot}-shot")
-    # a setting is a combination of task and shot, e.g. 1-shot colon
+    # a setting is a combination of task and shot, e.g. colon/1-shot
+    # within such a setting we then also have different experiments -> colon/1-shot/...exp1...
     try:
         setting_run_dirs = os.listdir(setting_directory)
     except Exception:
         return None, -1
 
-    best_score = 0
-    best_run = None
+    setting_run_dirs = filter_directories_by_exp(setting_run_dirs, exp)
+
+    run_score_list = []
 
     for run_dir in setting_run_dirs:
-        # skip if should be excluded
-        if exclude_model(run_dir):
-            print(f"--Excluding {task}/{shot}-shot/{run_dir}")
-            continue
-
         print(f"checking {task}/{shot}-shot/{run_dir}")
         run_dir_path = os.path.join(setting_directory, run_dir)
-
-        # skip if no checkpoint
-        ckpt_file = get_ckpt_file_from_run_dir(run_dir_path)
-        if ckpt_file is None:
-            continue
 
         # skip if no event file
         event_file = get_event_file_from_run_dir(run_dir_path)
@@ -127,25 +126,32 @@ def get_best_run_dir(task, shot, metric):
         if score == -1:
             continue
 
-        if score > best_score:
-            best_score = score
-            best_run = run_dir
-    return best_run, best_score
+        run_score_list.append((run_dir, score))
+
+    run_score_list.sort(key=lambda x: x[1], reverse=True)
+
+    return run_score_list[:max(5, len(run_score_list))]
 
 
 report = []
-best_runs = []
-scores = []
+best_settings = {}
 
 for task in tasks:
+    best_settings[task] = {}
+
     for shot in shots:
-        best_run, best_score = get_best_run_dir(task, shot, metric)
-        scores.append(best_score)
-        if best_run is None:
-            report.append(f"| {shot}-shot_{task}\t No run found")
-        else:
-            report.append(f"| {shot}-shot_{task}\t{metric}: {best_score}\t{best_run}")
-            best_runs.append(os.path.join(task, f"{shot}-shot", best_run))
+        best_settings[task][shot] = {}
+
+        for exp in exps:
+            best_runs = get_5_best_exp_run_dirs(task=task, shot=shot, exp=exp, metric=metric)
+            best_settings[task][shot][exp] = best_runs
+
+            if len(best_runs) == 0:
+                report.append(f"| {task}/{shot}-shot/exp{exp}\t No run found")
+            else:
+                for run in best_settings[task][shot][exp]:
+                    report.append(f"| {task}/{shot}-shot/exp{exp}\t{metric}: {run[1]}\t{run[0]}")
+                    #best_runs.append(os.path.join(task, f"{shot}-shot", best_run))
 
 print("")
 print("---------------------------------------------------------------------------------------------------------------")
@@ -156,59 +162,60 @@ for line in report:
         print(colored(line, 'red'))
     else:
         print(line)
+#print("---------------------------------------------------------------------------------------------------------------")
+#print(f"| Mean score: {sum(scores) / len(scores)}")
 print("---------------------------------------------------------------------------------------------------------------")
-print(f"| Mean score: {sum(scores) / len(scores)}")
-print("---------------------------------------------------------------------------------------------------------------")
-if args.eval:
-    exit()
 
-# create dir for submission and config
-date_pattern = datetime.now().strftime("%d-%m_%H-%M-%S")
-submission_dir = os.path.join("submissions", date_pattern)
-configs_dir = os.path.join(submission_dir, "configs")
-predictions_dir = os.path.join(submission_dir, "predictions")
+# if args.eval:
+#     exit()
 
-os.makedirs(submission_dir)
-with open(os.path.join(submission_dir, "report.txt"), "w") as file:
-    txt_report = ""
-    for line in report:
-        txt_report += line + "\n"
-    file.write(txt_report)
-
-os.makedirs(configs_dir)
-os.makedirs(predictions_dir)
-
-bash_script = "#!/bin/bash\n"
-for given_run_path in best_runs:
-    scratch_repo_path = os.path.join("/scratch", "medfm", "medfm-challenge")
-
-    task = given_run_path.split(os.sep)[0]
-    if task.__contains__("-"):
-        task = task.split("-")[0]
-    shot = given_run_path.split(os.sep)[1]
-
-    given_run_path = os.path.join("work_dirs", given_run_path)
-    path_components = given_run_path.split(os.sep)
-    run_dir = os.path.join(*path_components[:4])
-    run_dir = os.path.join(scratch_repo_path, run_dir)
-
-    config_filename = [file for file in os.listdir(run_dir) if file.endswith(".py")][0]
-    checkpoint_filename = [file for file in os.listdir(run_dir) if file.endswith(".pth") and file.__contains__("best")][0]
-
-    config_path = os.path.join(run_dir, config_filename)
-    checkpoint_path = os.path.join(run_dir, checkpoint_filename)
-    images_path = os.path.join(scratch_repo_path, "data", "MedFMC_val", task, "images")
-    csv_name = f"{task}_{shot}_submission.csv"
-    out_path = os.path.join(predictions_dir, csv_name)
-
-    # copy config into submission directory
-    shutil.copy(config_path, configs_dir)
-    command = f"python tools/infer.py {config_path} {checkpoint_path} {images_path} --out {out_path}\n"
-    bash_script += command
-
-print(f"Saved respective configs to {configs_dir}")
-print("Created infer.sh")
-print(f"Run ./infer.sh to create prediction files in {predictions_dir}")
-with open("infer.sh", "w") as file:
-    file.write(bash_script)
-os.chmod("infer.sh", 0o755)
+# # create dir for submission and config
+# date_pattern = datetime.now().strftime("%d-%m_%H-%M-%S")
+# submission_dir = os.path.join("submissions", date_pattern)
+# configs_dir = os.path.join(submission_dir, "configs")
+# predictions_dir = os.path.join(submission_dir, "predictions")
+#
+# os.makedirs(submission_dir)
+# with open(os.path.join(submission_dir, "report.txt"), "w") as file:
+#     txt_report = ""
+#     for line in report:
+#         txt_report += line + "\n"
+#     file.write(txt_report)
+#
+# os.makedirs(configs_dir)
+# os.makedirs(predictions_dir)
+#
+# bash_script = "#!/bin/bash\n"
+# for given_run_path in best_runs:
+#     scratch_repo_path = os.path.join("/scratch", "medfm", "medfm-challenge")
+#
+#     task = given_run_path.split(os.sep)[0]
+#     if task.__contains__("-"):
+#         task = task.split("-")[0]
+#     shot = given_run_path.split(os.sep)[1]
+#
+#     given_run_path = os.path.join("work_dirs", given_run_path)
+#     path_components = given_run_path.split(os.sep)
+#     run_dir = os.path.join(*path_components[:4])
+#     run_dir = os.path.join(scratch_repo_path, run_dir)
+#
+#     config_filename = [file for file in os.listdir(run_dir) if file.endswith(".py")][0]
+#     checkpoint_filename = [file for file in os.listdir(run_dir) if file.endswith(".pth") and file.__contains__("best")][0]
+#
+#     config_path = os.path.join(run_dir, config_filename)
+#     checkpoint_path = os.path.join(run_dir, checkpoint_filename)
+#     images_path = os.path.join(scratch_repo_path, "data", "MedFMC_val", task, "images")
+#     csv_name = f"{task}_{shot}_submission.csv"
+#     out_path = os.path.join(predictions_dir, csv_name)
+#
+#     # copy config into submission directory
+#     shutil.copy(config_path, configs_dir)
+#     command = f"python tools/infer.py {config_path} {checkpoint_path} {images_path} --out {out_path}\n"
+#     bash_script += command
+#
+# print(f"Saved respective configs to {configs_dir}")
+# print("Created infer.sh")
+# print(f"Run ./infer.sh to create prediction files in {predictions_dir}")
+# with open("infer.sh", "w") as file:
+#     file.write(bash_script)
+# os.chmod("infer.sh", 0o755)
