2717948594a882695cfd43793e9e8ab53d29e566
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2023-08-31 23:58:30 +0200

    update swinv2 endo cfgs

--- gridsearch/configs/config.py
+++ gridsearch/configs/config.py
@@ -1,11 +1,11 @@
 OVERRIDE = {
-    'model': ["resnet101"],
-    'dataset': ["chest"],
+    'model': ["swinv2-b"],
+    'dataset': ["endo", "chest"],
     'shot': [1, 5, 10],
     #'exp_num': [2],
     'lr': [1e-6],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
     #'train_bs': [32],
-    'seed': [0, 1, 2, 3, 4]
+    'seed': [0, 1, 2]
 }
 
 SETTINGS = {
