6e1eb6d519987cbad4d0959ac966a2ffb0d7215c
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2023-08-31 17:33:06 +0200

    update prep_infer

--- prep_infer_with_exp.py
+++ prep_infer_with_exp.py
@@ -255,13 +255,12 @@ for run_path in best_runs:
     command = f"python tools/infer.py {config_path} {checkpoint_path} {images_path} --out {out_path}\n"
     commands.append(command)
 
-print(f"Saved respective configs to {configs_dir}")
-print(commands)
+print(f"Saved respective configs to {configs_dir}\n")
 # Display the generated commands
 print("Generated Infer Commands:")
 
-# for command in commands:
-#     print(command)
+for command in commands:
+    print(command)
 
 # Prompt the user
 user_input = input(f"Do you want to run {len(commands)} commands on the cluster? (yes/no): ")
