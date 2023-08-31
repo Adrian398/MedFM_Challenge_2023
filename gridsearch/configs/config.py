072c1d7977184a1a0502e36785f68f7e93d90c45
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2023-08-31 23:36:23 +0200

    add resnet101 chest configs

--- gridsearch/configs/config.py
+++ gridsearch/configs/config.py
@@ -1,9 +1,9 @@
 OVERRIDE = {
     'model': ["resnet101"],
-    'dataset': ["endo"],
-    'shot': [5, 10],
+    'dataset': ["chest"],
+    'shot': [1, 5, 10],
     #'exp_num': [2],
-    'lr': [5e-6],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
+    'lr': [1e-6],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
     #'train_bs': [32],
     'seed': [0, 1, 2, 3, 4]
 }
