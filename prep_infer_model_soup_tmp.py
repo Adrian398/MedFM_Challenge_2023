291b09a3eaaec2d9fa36d9a745ac073ac1becd26
Micha Nowak <gonderjake@gmail.com>
2023-09-02 01:42:02 +0200

    update

--- prep_infer_model_soup_tmp.py
+++ prep_infer_model_soup_tmp.py
@@ -10,12 +10,22 @@ from mmengine.runner import Runner
 def get_sd(state_dicts, alphal):
     sd = {}  # Initialize an empty dictionary
     for k in state_dicts[0]['state_dict'].keys():
-        # print(k)
-        # print(state_dicts[0][k])
         sd[k] = state_dicts[0]['state_dict'][k].clone() * alphal[0]
+
+    # Print a few weights from the first state dict for comparison.
+    print("Initial weights (first state dict):")
+    print("Key:", list(state_dicts[0]['state_dict'].keys())[0])
+    print(state_dicts[0]['state_dict'][list(state_dicts[0]['state_dict'].keys())[0]])
+
     for i in range(1, len(state_dicts)):
         for k in state_dicts[i]['state_dict'].keys():
             sd[k] = sd[k] + state_dicts[i]['state_dict'][k].clone() * alphal[i]
+
+    # Print a few combined weights for comparison
+    print("Combined weights:")
+    print("Key:", list(sd.keys())[0])
+    print(sd[list(sd.keys())[0]])
+
     return sd
 
 
@@ -108,6 +118,7 @@ ranked_candidates.sort(key=lambda x: -val_results[x])
 current_best = val_results[ranked_candidates[0]]
 best_ingredients = ranked_candidates[:1]
 print("Starting model soup search")
+# this for loop will always create the same model because
 for i in range(1, len(state_dicts)):
     # add current index to the ingredients
     ingredient_indices = best_ingredients + [ranked_candidates[i]]
@@ -118,6 +129,7 @@ for i in range(1, len(state_dicts)):
 
     # benchmark and conditionally append
     sd = get_sd(state_dicts, alphal)
+    exit()
     folder_path = checkpoint_filenames[0].split("-shot")[0] + "-shot/modelsoup"
 
     if not os.path.exists(folder_path):
