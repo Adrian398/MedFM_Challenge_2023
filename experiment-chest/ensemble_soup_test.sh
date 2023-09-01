53886765336a51bb79de1ab3b346bcaf3442eca8
AmarHek <amar.hekalo@uni-wuerzburg.de>
2023-09-01 16:56:09 +0200

    added lr to script

--- experiment-chest/ensemble_soup_test.sh
+++ experiment-chest/ensemble_soup_test.sh
@@ -12,6 +12,7 @@ log_output="/home/ls6/hekalo/job_output/medfm-%j.out"
 exp_num=1
 exp_suffix="ensemble_soup_test"
 seed=1337
+lr=1e-6
 
 configs=(
   "configs/clip-b_vpt/5-shot_chest.py"
@@ -28,13 +29,13 @@ batch_sizes=(1 2 4 8)
 
 for config in "${configs[@]}"; do
   for batch_size in "${batch_sizes[@]}"; do
-    sbatch -p ls6 --gres=gpu:1 --nodelist=gpu8a --wrap="python tools/train.py $config --train_bs=$batch_size --seed=$seed --exp_num=$exp_num --remove_timestamp --exp_suffix=$exp_suffix" -o $log_output
+    sbatch -p ls6 --gres=gpu:1 --nodelist=gpu8a --wrap="python tools/train.py $config --lr=$lr --train_bs=$batch_size --seed=$seed --exp_num=$exp_num --remove_timestamp --exp_suffix=$exp_suffix" -o $log_output
   done
 done
 
 for config_2 in "${configs_2[@]}"; do
   for batch_size in "${batch_sizes[@]}"; do
-    sbatch -p ls6 --gres=gpu:rtx3090:1 --wrap="python tools/train.py $config_2 --train_bs=$batch_size --seed=$seed --exp_num=$exp_num --remove_timestamp --exp_suffix=$exp_suffix" -o $log_output
+    sbatch -p ls6 --gres=gpu:rtx3090:1 --wrap="python tools/train.py $config_2 --train_bs=$batch_size --lr=$lr --seed=$seed --exp_num=$exp_num --remove_timestamp --exp_suffix=$exp_suffix" -o $log_output
   done
 done
 
