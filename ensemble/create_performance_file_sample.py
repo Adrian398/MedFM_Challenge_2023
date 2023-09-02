2da4be19030fa3238677b2d735d978c5ebd1aefa
Micha Nowak <gonderjake@gmail.com>
2023-09-02 23:58:28 +0200

    update

--- ensemble/create_performance_file_sample.py
+++ ensemble/create_performance_file_sample.py
@@ -1,16 +1,21 @@
 import json
+import argparse
 from mmengine.config import Config
 from mmengine.runner import Runner
 
-
 if __name__ == "__main__":
-    # todo read from run directory
-    checkpoint_path = "work_dirs/endo/1-shot/resnet101_bs4_lr1e-06_exp3__20230902-125110/best_Aggregate_epoch_5.pth"
-    config_path = "configs/resnet101/1-shot_endo.py"
+    parser = argparse.ArgumentParser(description="Test a model with given checkpoint and config.")
 
-    cfg = Config.fromfile(config_path)
-    cfg.load_from = checkpoint_path
+    # Adding command-line arguments
+    parser.add_argument("--checkpoint_path", type=str, required=True, help="Path to the model checkpoint.")
+    parser.add_argument("--config_path", type=str, required=True, help="Path to the model configuration.")
+    parser.add_argument("--output_path", type=str, default="performance.json", help="Path to save the output metrics. Default is 'performance.json'.")
+
+    args = parser.parse_args()
+
+    cfg = Config.fromfile(args.config_path)
+    cfg.load_from = args.checkpoint_path
     runner = Runner.from_cfg(cfg)
     metrics = runner.test()
-    with open('performance.json', 'w') as f:
+    with open(args.output_path, 'w') as f:
         json.dump(metrics, f, indent=4)
