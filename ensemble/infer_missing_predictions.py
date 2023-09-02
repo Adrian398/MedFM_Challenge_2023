a1bb7777dd03aaa16a60aea40127ea9f0bdbb8d5
Marcel Roth <MarcelRoth100@aol.com>
2023-09-02 20:12:49 +0200

    update

--- ensemble/infer_missing_predictions.py
+++ ensemble/infer_missing_predictions.py
@@ -167,8 +167,9 @@ if __name__ == "__main__":  # Important when using multiprocessing
 
     for model_dir in model_dirs:
         # Extract task, shot, and model_dir_name from the model_dir path
-        task, shot_with_suffix, _ = model_dir.split(os.sep, 2)
-
+        model_dir_split = model_dir.split("work_dirs")
+        print(model_dir_split)
+        #task, shot, exp = model_dir_split[]
         print(model_dir.split(os.sep, 2))
         print(task, shot_with_suffix)
 
