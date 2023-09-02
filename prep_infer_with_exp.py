e4364da833994147751d1b03204d6c1099e4e992
Marcel Roth <MarcelRoth100@aol.com>
2023-09-02 02:28:47 +0200

    update prep infer

--- prep_infer_with_exp.py
+++ prep_infer_with_exp.py
@@ -29,8 +29,8 @@ def run_commands_on_cluster(commands, gpu=None, delay_seconds=1):
         gpus = ['rtx3090']
     elif gpu == '8a':
         gpus = ['rtx2080ti']
-    elif gpu is None:
-        gpus = ['rtx4090', 'rtx3090', 'rtx3090']
+    elif gpu is 'all':
+        gpus = ['rtx4090', 'rtx3090', 'rtx4090', 'rtx3090']
     else:
         raise ValueError(f'Invalid gpu type {gpu}.')
 
