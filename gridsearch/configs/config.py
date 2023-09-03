94da2e8fd8b9b004fee0042bf53f8f08dd780209
Marcel Roth <MarcelRoth100@aol.com>
2023-09-04 00:34:13 +0200

    update colon cfgs

--- gridsearch/configs/config.py
+++ gridsearch/configs/config.py
@@ -1,8 +1,8 @@
 OVERRIDE = {
     'model': ["convnext-v2-b"],
     'dataset': ["colon"],
-    'shot': [1],
-    'exp_num': [1],
+    #'shot': [1],
+    #'exp_num': [1],
     'lr': [1e-6],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
     'train_bs': [1, 2],
     'seed': [0, 1, 2, 3]
