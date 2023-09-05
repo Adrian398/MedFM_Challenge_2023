44745a829559caf5caffbebf5dc514a4c2aa9a8a
Marcel Roth <MarcelRoth100@aol.com>
2023-09-06 00:31:08 +0200

    update

--- gridsearch/configs/config.py
+++ gridsearch/configs/config.py
@@ -1,7 +1,7 @@
 OVERRIDE = {
     'model': ["resnet101"],
     'dataset': ["colon"],
-    'shot': [5, 10],
+    #'shot': [5, 10],
     #'exp_num': [1],
     'lr': [1e-6],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
     'train_bs': [1, 2, 8, 16, 32],
