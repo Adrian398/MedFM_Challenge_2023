dbd7ba71df283e1bd6aa47f4c9f37acb7112e4e2
Marcel Roth <MarcelRoth100@aol.com>
2023-09-06 00:21:30 +0200

    update

--- gridsearch/configs/config.py
+++ gridsearch/configs/config.py
@@ -1,14 +1,14 @@
 OVERRIDE = {
-    'model': ["convnext-v2-b"],
+    'model': ["resnet101"],
     'dataset': ["colon"],
     #'shot': [1],
     #'exp_num': [1],
-    'lr': [5e-5],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
-    'train_bs': [1],
+    'lr': [1e-6],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
+    'train_bs': [1, 2, 8, 16, 32],
     'seed': [0]
 }
 
 SETTINGS = {
-    #'exp_suffix': "exp+seed_test_",
+    'exp_suffix': "bs_test_",
     'log_level': "INFO"
 }
