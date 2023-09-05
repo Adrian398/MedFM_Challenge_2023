fb632449413aa949b6835db425eb25a436fa3ed5
Marcel Roth <MarcelRoth100@aol.com>
2023-09-06 00:13:46 +0200

    update

--- tools/compare/comparison_dashboard.py
+++ tools/compare/comparison_dashboard.py
@@ -1,8 +1,6 @@
 import json
-
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-
 
 def init_settings():
     tasks_ = ["colon", "chest", "endo"]
@@ -11,7 +9,6 @@ def init_settings():
     metrics_ = ["Acc_metric", "mAP_metric"]
     return tasks_, shots_, exps_, metrics_, [task + "_" + shot for task in tasks_ for shot in shots_]
 
-
 def load_team_data(team1, team2):
     with open(f"{team1}.json", 'r') as src_file:
         src_team_data = json.load(src_file)
@@ -19,12 +16,12 @@ def load_team_data(team1, team2):
         trg_team_data = json.load(trg_file)
     return src_team_data, trg_team_data
 
-
-def get_values_from_data(data, exp, setting, metric_key):
-    """Fetch the metric values from the provided data."""
-    metric_data = data['task'][exp][setting].get(metric_key)
-    if metric_data:
-        return float(metric_data)
+def get_values_from_data(data, exp, setting, metric_keys):
+    """Fetch the metric values from the provided data for multiple potential metric keys."""
+    for metric_key in metric_keys:
+        metric_data = data['task'][exp][setting].get(metric_key)
+        if metric_data:
+            return float(metric_data)
     return None
 
 
@@ -32,19 +29,19 @@ def compare_results(team1, team2):
     comparison_result = {}
     team1_name, team1_data = team1.values()
     team2_name, team2_data = team2.values()
-    metrics = ['Acc_metric', 'mAP_metric']
 
     for exp in exps:
         comparison_result[exp] = {}
 
         for setting in settings:
             comparison_result[exp][setting] = {}
+            metric_keys = task2metric[next(task for task in task2metric if task in setting)]
 
-            for metric in metrics:
-                team1_val = team1_data['task'][exp][setting].get(metric)
-                team2_val = team2_data['task'][exp][setting].get(metric)
+            for metric_key in metric_keys:
+                team1_val = team1_data['task'][exp][setting].get(metric_key)
+                team2_val = team2_data['task'][exp][setting].get(metric_key)
                 if team1_val is not None and team2_val is not None:
-                    comparison_result[exp][setting][metric] = {team1_name: team1_val, team2_name: team2_val}
+                    comparison_result[exp][setting][metric_key] = {team1_name: team1_val, team2_name: team2_val}
     return comparison_result
 
 
@@ -52,8 +49,11 @@ source_team_name = "uniwue_validation_greedy"
 target_team_name = "uniwue_validation_expert_ensemble"
 
 tasks, shots, exps, metrics, settings = init_settings()
-
-task2metric = {'colon': metrics[0], 'chest': metrics[1], 'endo': metrics[1]}
+task2metric = {
+    'colon': ['Acc_metric', 'ACC_metric'],
+    'chest': ['mAP_metric'],
+    'endo': ['mAP_metric']
+}
 
 source_data, target_data = load_team_data(team1=source_team_name, team2=target_team_name)
 result = compare_results(team1={'name': source_team_name, 'data': source_data},
@@ -63,10 +63,10 @@ result = compare_results(team1={'name': source_team_name, 'data': source_data},
 fig = make_subplots(rows=len(settings), cols=1, subplot_titles=settings, vertical_spacing=0.05, shared_xaxes=True)
 
 for idx, setting in enumerate(settings):
-    metric_key = task2metric[next(task for task in task2metric if task in setting)]
+    metric_keys = task2metric[next(task for task in task2metric if task in setting)]
 
-    source_values = [get_values_from_data(source_data, exp, setting, metric_key) for exp in exps]
-    target_values = [get_values_from_data(target_data, exp, setting, metric_key) for exp in exps]
+    source_values = [get_values_from_data(source_data, exp, setting, metric_keys) for exp in exps]
+    target_values = [get_values_from_data(target_data, exp, setting, metric_keys) for exp in exps]
 
     # Add traces to the subplot
     fig.add_trace(go.Bar(x=exps,
@@ -84,11 +84,12 @@ for idx, setting in enumerate(settings):
                          ), row=idx + 1, col=1)
 
 yaxis_titles = {
-    f'yaxis{idx if idx > 1 else ""}_title': task2metric[task]
+    f'yaxis{idx if idx > 1 else ""}_title': task2metric[task][0]
     for idx, setting in enumerate(settings, 1)
     for task in task2metric
     if task in setting
 }
+
 
 fig.update_layout(
     title_text="Comparison for two evaluation submissions",
