450204ff3774c971e67f61bf5735e91d4abb25f2
Adrian39 <adrian@stellaris.de>
2023-09-06 01:46:49 +0200

    add create preprocess data

--- configs/datasets/endoscopy_pre_processed.py
+++ configs/datasets/endoscopy_pre_processed.py
@@ -29,7 +29,7 @@ train_dataloader = dict(
     num_workers=2,
     dataset=dict(
         type=dataset_type,
-        data_prefix='/scratch/medfm/medfm-challenge/data/MedFMC_train/endo/images_pre_processed',
+        data_prefix='/scratch/medfm/medfm-challenge/data/MedFMC_train/endo/pre_processed_images',
         ann_file='data_anns/MedFMC/endo/train_20.txt',
         pipeline=train_pipeline),
     sampler=dict(type='DefaultSampler', shuffle=True),
@@ -40,7 +40,7 @@ val_dataloader = dict(
     num_workers=2,
     dataset=dict(
         type=dataset_type,
-        data_prefix='/scratch/medfm/medfm-challenge/data/MedFMC_train/endo/images_pre_processed',
+        data_prefix='/scratch/medfm/medfm-challenge/data/MedFMC_train/endo/pre_processed_images',
         ann_file='data_anns/MedFMC/endo/val_20.txt',
         pipeline=test_pipeline),
     sampler=dict(type='DefaultSampler', shuffle=False),
@@ -52,7 +52,7 @@ test_dataloader = dict(
     dataset=dict(
         # replace `data/val` with `data/test` for standard test
         type=dataset_type,
-        data_prefix='/scratch/medfm/medfm-challenge/data/MedFMC_train/endo/images_pre_processed',
+        data_prefix='/scratch/medfm/medfm-challenge/data/MedFMC_train/endo/pre_processed_images',
         ann_file='data_anns/MedFMC/endo/test_WithLabel.txt',
         pipeline=test_pipeline),
     sampler=dict(type='DefaultSampler', shuffle=False),
