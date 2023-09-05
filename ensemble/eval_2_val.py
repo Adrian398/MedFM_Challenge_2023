4b24a677f03932822dd2dc8918b5e6623778206d
Marcel Roth <MarcelRoth100@aol.com>
2023-09-05 23:10:07 +0200

    update

--- ensemble/eval_2_val.py
+++ ensemble/eval_2_val.py
@@ -4,7 +4,7 @@ import os
 def construct_model_paths(report):
     lines = [line for line in report if any(task in line for task in TASKS)]
 
-    model_paths = []
+    model_infos = []
     for line in lines:
         parts = line.split('\t')
 
@@ -14,29 +14,45 @@ def construct_model_paths(report):
         # Extract task, shot, and exp number
         task = task_shot_exp[0].strip('| ').strip()
         shot = task_shot_exp[1].strip()
-        exp = task_shot_exp[2].split("exp")[1].strip()  # extract the number after "exp"
+        exp = task_shot_exp[2].strip()
 
-        # Constructing the path
-        path = os.path.join(EVAL_BASE_PATH, "work_dirs", task, shot, model_name)
-        model_paths.append(path)
-    return model_paths
+        model_infos.append({
+            'model_name': model_name,
+            'task': task,
+            'shot': shot,
+            'exp': exp,
+        })
+    return model_infos
 
 
 # ================================================================================
-TIMESTAMP = '02-09_00-32-41'
 SCRATCH_BASE_PATH = '/scratch/medfm/medfm-challenge'
-EVAL_BASE_PATH = 'submissions/evaluation'
-EVAL_FOLDER_PATH = os.path.join(EVAL_BASE_PATH, TIMESTAMP)
-EVAL_REPORT_PATH = os.path.join(EVAL_FOLDER_PATH, 'report.txt')
+VAL_TARGET_PATH = 'ensemble/validation'
+EVAL_BASE_PATH = 'submission/evaluation'
 TASKS = ["colon", "endo", "chest"]
 # ================================================================================
+
+# TODO: Read TIMESTAMP during runtime
+TIMESTAMP = '02-09_00-32-41'
+EVAL_REPORT_PATH = os.path.join(EVAL_BASE_PATH, TIMESTAMP, 'report.txt')
 
 
 with open(EVAL_REPORT_PATH, 'r') as f:
     report_content = f.readlines()
 
-model_paths = construct_model_paths(report_content)
+model_infos = construct_model_paths(report_content)
 
-for path in model_paths:
+for path in model_infos:
     print(path)
 
+for model_name, task, shot, exp in model_infos:
+    # Search for validation prediction csv and validate it
+    file_name = f"{task}_{shot}_validation.csv"
+    source_path = os.path.join(SCRATCH_BASE_PATH, 'work_dirs', task, shot, model_name, file_name)
+    print(source_path)
+
+    # Construct target path and ensure that the directory exists
+    target_path = os.path.join(VAL_TARGET_PATH, TIMESTAMP, 'result', exp)
+    os.makedirs(target_path, exist_ok=True)
+    print(target_path)
+    exit()
