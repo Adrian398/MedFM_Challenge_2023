dbe28c63e3aa830a1e059973bfe751bbbc150562
Amar Hekalo <amar.hekalo@uni-wuerzburg.de>
2023-09-06 23:14:24 +0200

    added efficientformer configs (unfortunately not useable)

--- configs/efficientformer/1-shot_chest.py
+++ configs/efficientformer/1-shot_chest.py
@@ -2,36 +2,29 @@ _base_ = [
     '../datasets/chest.py',
     '../schedules/chest.py',
     'mmpretrain::_base_/default_runtime.py',
-    'mmpretrain::_base_/models/densenet/densenet121.py',
+    'mmpretrain::_base_/models/efficientformer-l1.py',
     '../custom_imports.py',
 ]
 
-# Pre-trained Checkpoint Path
-checkpoint = 'https://download.openmmlab.com/mmclassification/v0/densenet/densenet121_4xb256_in1k_20220426-07450f99.pth'  # noqa
-
-lr = 1e-4
+lr = 1e-5
 train_bs = 4
 val_bs = 128
 dataset = 'chest'
-model_name = 'densenet121'
+model_name = 'efficientformer'
+arch = "l1"
 exp_num = 1
 nshot = 1
 
-run_name = f'{model_name}_bs{train_bs}_lr{lr}_exp{exp_num}'
+run_name = f'{model_name}_{arch}_bs{train_bs}_lr{lr}_exp{exp_num}'
 work_dir = f'work_dirs/{dataset}/{nshot}-shot/{run_name}'
 
 model = dict(
     backbone=dict(
-        init_cfg=dict(type='Pretrained', checkpoint=checkpoint, prefix='backbone')
+        arch=arch
     ),
-    neck=None,
     head=dict(
-        type='CSRAClsHead',
-        num_classes=19,
-        in_channels=1024,
-        num_heads=1,
-        lam=0.1,
-        loss=dict(type='CrossEntropyLoss', loss_weight=1.0)))
+        type='EfficientFormerClsHead',
+        num_classes=19))
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
@@ -44,12 +37,7 @@ train_pipeline = [
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='Resize', scale=224),
-    dict(
-        type='PackInputs',
-        # `gt_label_difficult` is needed for VOC evaluation
-        meta_keys=('sample_idx', 'img_path', 'ori_shape', 'img_shape',
-                   'scale_factor', 'flip', 'flip_direction',
-                   'gt_label_difficult')),
+    dict(type='PackInputs')
 ]
 
 train_dataloader = dict(
@@ -75,24 +63,7 @@ default_hooks = dict(
     logger=dict(interval=10),
 )
 
-optimizer = dict(betas=(0.9, 0.999), eps=1e-08, lr=lr, type='AdamW', weight_decay=0.05)
-
-optim_wrapper = dict(
-    optimizer=optimizer,
-    paramwise_cfg=dict(
-        norm_decay_mult=0.0,
-        bias_decay_mult=0.0,
-        flat_decay_mult=0.0,
-        custom_keys={
-            '.absolute_pos_embed': dict(decay_mult=0.0),
-            '.relative_position_bias_table': dict(decay_mult=0.0)
-        }),
-)
-
-param_scheduler = [
-    dict(by_epoch=True, end=1, start_factor=1, type='LinearLR'),
-    dict(begin=1, by_epoch=True, eta_min=1e-05, type='CosineAnnealingLR'),
-]
+optim_wrapper = dict(optimizer=dict(lr=lr))
 
 visualizer = dict(type='Visualizer', vis_backends=[dict(type='TensorboardVisBackend')])
 
