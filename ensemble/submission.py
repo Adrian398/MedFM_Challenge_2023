d120b66fee5c8415ccdd4e24a92207258e725c1e
Marcel Roth <MarcelRoth100@aol.com>
2023-09-08 17:00:08 +0200

    update

--- ensemble/submission.py
+++ ensemble/submission.py
@@ -352,7 +352,7 @@ def print_report_for_setting(full_model_list, task, shot, exp):
             model_view.append((model_info['name'], agg_name, agg_val, m1_name, m1_val, m2_name, m2_val))
 
     model_view.sort(key=lambda x: x[2])
-    max_char_length = max(len(path) for path, _, _ in model_view)
+    max_char_length = max(len(path) for path, _, _, _, _, _, _ in model_view)
 
     for model_path_rel, agg_name, agg_val, m1_name, m1_val, m2_name, m2_val in model_view:
         print(f"Model: {model_path_rel:{max_char_length}} {agg_name}: {agg_val:.4f}  "
