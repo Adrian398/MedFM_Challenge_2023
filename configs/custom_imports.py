f9a57c20fdda38b67b37bf8cc73745b7bd8527fc
Adrian39 <adrian@stellaris.de>
2023-09-09 19:19:20 +0200

    init_commit

--- configs/custom_imports.py
+++
@@ -1,7 +0,0 @@
-custom_imports = dict(
-    imports=[
-        'medfmc.datasets.medical_datasets',
-        'medfmc.evaluation.metrics.auc',
-        'medfmc.models'
-    ],
-    allow_failed_imports=False)
--- run.sh
+++
@@ -1,4 +0,0 @@
-export WORKDIR=/medfmc_exp
-cd $WORKDIR
-export PYTHONPATH=$PWD:$PYTHONPATH
-python tools/train.py configs/densenet/dense121_chest.py --work-dir work_dirs/temp/
