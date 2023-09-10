958df2a7a938e8319ae2a6f9404f2042e522157e
Marcel Roth <MarcelRoth100@aol.com>
2023-09-10 11:57:32 +0200

    update

--- gridsearch/configs/config.py
+++ gridsearch/configs/config.py
@@ -5,7 +5,7 @@ OVERRIDE = {
     #'exp_num': [1],
     'lr': [1e-6],  # Start learning rate that increases up to 1e-5 (until max_epochs) with cosine annealing
     'train_bs': [1, 2],
-    'seed': [1, 42]
+    'seed': [42]
 }
 
 SETTINGS = {
