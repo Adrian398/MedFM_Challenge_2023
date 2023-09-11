5b9654eec6095d301b6c10f369fc01d74c05eab6
Marcel Roth <MarcelRoth100@aol.com>
2023-09-11 16:47:08 +0200

    add todo

--- ensemble/gridsearch/task_submission.py
+++ ensemble/gridsearch/task_submission.py
@@ -833,6 +833,7 @@ TASKS = ["colon", "endo", "chest"]
 if __name__ == "__main__":
     root_dir = "/scratch/medfm/medfm-challenge/work_dirs"
 
+    ENSEMBLE_STRATEGIES = ["stacking"]
     TASKS = ["endo"]
     SUBMISSION_TYPES = ["validation"]
 
