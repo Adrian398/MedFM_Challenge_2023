7029c160cee86b6514f13838ce20f4a68dd9629b
Marcel Roth <MarcelRoth100@aol.com>
2023-09-12 09:16:20 +0200

    update

--- gridsearch/configs/config.py
+++ gridsearch/configs/config.py
@@ -2,7 +2,7 @@ OVERRIDE = {
     'model': ["pre_processed_swin-b_vpt"],
     'dataset': ["endo"],
     'shot': [1],
-    'exp_num': [3],
+    #'exp_num': [3],
     'lr': [1e-4],
     'train_bs': [1, 2, 16],
     #'seed': [0, 1, 2, 3, 4, 5, 6, 12, 42]
