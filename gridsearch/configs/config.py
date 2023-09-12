a61187c7976acd4dd8c0b3ed068e0fa4564eb586
Marcel Roth <MarcelRoth100@aol.com>
2023-09-12 09:26:08 +0200

    update

--- gridsearch/configs/config.py
+++ gridsearch/configs/config.py
@@ -3,9 +3,9 @@ OVERRIDE = {
     'dataset': ["endo"],
     'shot': [1],
     #'exp_num': [3],
-    'lr': [1e-4],
-    'train_bs': [1, 2, 16],
-    #'seed': [0, 1, 2, 3, 4, 5, 6, 12, 42]
+    'lr': [5e-4],
+    'train_bs': [8],
+    'seed': [0, 1, 2, 3, 4, 5, 6]
 }
 
 SETTINGS = {
