6d21fd3726a13ac8d639d377efb87a37eb9cebb0
Marcel Roth <MarcelRoth100@aol.com>
2023-09-13 13:49:08 +0200

    update

--- ensemble/utils/clear_redundant_checkpoints.py
+++ ensemble/utils/clear_redundant_checkpoints.py
@@ -89,7 +89,7 @@ def get_non_valid_model_dirs(task, shot):
         best_checkpoints = get_file_from_directory(abs_model_dir, ".pth", "best")
 
         if len(best_checkpoints) > 1:
-            print(colored(f"More than one 'best' checkpoint found in {model_dir}", 'yellow'))
+            print(colored(f"More than one 'best' checkpoint found in {abs_model_dir}", 'yellow'))
             model_dirs.append(model_dir)
             continue
 
@@ -104,7 +104,7 @@ def get_non_valid_model_dirs(task, shot):
         for chkpt_file in checkpoint_files:
             model_in_gb = os.path.getsize(os.path.join(abs_model_dir, chkpt_file)) / (1024 ** 3)
             total_size_gb += model_in_gb  # Convert bytes to GB
-            print(model_dir, model_in_gb)
+        print(f"Setting {task, f'{shot}-shot'}:", model_dir, f"{total_size_gb:.2f}")
 
     return model_dirs, total_size_gb
 
