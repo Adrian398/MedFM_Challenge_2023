53737df80b2a046fa2b6fafaf99a4c0e70c07f30
Marcel Roth <MarcelRoth100@aol.com>
2023-09-14 16:04:34 +0200

    add weighted expert-per-class

--- ensemble/gridsearch/task_submission.py
+++ ensemble/gridsearch/task_submission.py
@@ -982,12 +982,12 @@ COLOR_GRADIENTS = {
 
 if __name__ == "__main__":
     # 1st Level Params
-    SUBM_TYPES = ["validation"]
+    #SUBM_TYPES = ["validation"]
 
     # 2nd Level Params
-    TASKS = ["endo"]
-    SHOTS = ["1-shot"]
-    EXPS = ["exp1"]
+    #TASKS = ["endo"]
+    #SHOTS = ["1-shot"]
+    #EXPS = ["exp1"]
 
     # 3rd Level Params
     ENSEMBLE_STRATEGIES = ["expert-per-task",
